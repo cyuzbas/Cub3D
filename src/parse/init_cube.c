249c12cf6221e9d45890196e34e01087b78cb680
oliviawanghao <oliviawanghao@gmail.com>
2023-03-20 17:50:26 +0100

    mlx_load_png leak fixed

--- src/parse/init_cube.c
+++ src/parse/init_cube.c
@@ -6,7 +6,7 @@
 /*   By: hwang <hwang@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/12 20:01:29 by hwang         #+#    #+#                 */
-/*   Updated: 2023/03/17 16:54:56 by cyuzbas       ########   odam.nl         */
+/*   Updated: 2023/03/20 17:47:44 by hwang         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,10 @@ void	init_colour(t_colour *colour)
 void	init_texture(t_cube *cube, t_texture *textures)
 {
 	textures->count = 0;
-	textures->no_tex = (mlx_texture_t *)malloc(sizeof(mlx_texture_t));
-	textures->so_tex = (mlx_texture_t *)malloc(sizeof(mlx_texture_t));
-	textures->we_tex = (mlx_texture_t *)malloc(sizeof(mlx_texture_t));
-	textures->ea_tex = (mlx_texture_t *)malloc(sizeof(mlx_texture_t));
-	if (!textures->no_tex || !textures->so_tex || \
-		!textures->we_tex || !textures->ea_tex)
-		put_error(cube, "Failed to initialize the wall texture!\n");
+	textures->no_tex = NULL;
+	textures->so_tex = NULL;
+	textures->we_tex = NULL;
+	textures->ea_tex = NULL;
 	textures->ceiling = (t_colour *)malloc(sizeof(t_colour));
 	if (!textures->ceiling)
 		put_error(cube, "Failed to initialize the ceiling colour!\n");
